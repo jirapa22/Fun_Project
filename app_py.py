a8fdbcf2979776174619c8452d2472ed0a91d189
jirapa22 <jirapa22panit@gmail.com>
2026-03-29 14:25:49 +0700

    app_py.py

--- app_py.py
+++ app_py.py
@@ -13,6 +13,6 @@ import random
 st.title('Test Streamlit')
 st.write('Hello Wold!')
 
-if st.button('Generate Random Number')
+if st.button('Generate Random Number'):
     random_number = random.randin(1,100)
     st.write(f'Random Number: {ramdom_number}')
